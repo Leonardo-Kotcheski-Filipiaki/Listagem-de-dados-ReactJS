2d3bcde7d88be508e5444d8ca402a73002029b03
Leonardo Kotcheski Filipiaki <duckgames875@gmail.com>
2024-02-19 22:04:49 -0300

    React na Prática class 1°

---
+++ src/App.tsx
@@ -0,0 +1,145 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query"
+import { Plus, Search, FileDown, MoreHorizontal, Filter } from "lucide-react"
+import { Control, Input } from "./components/ui/input"
+import { Button } from "./components/ui/button"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
+import { Pagination } from "./components/pagination"
+import { Header } from "./components/header"
+import { Tabs } from "./components/tabs"
+import { useSearchParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import useDebounceValue from "./hooks/use-debounce-value"
+
+export interface TagsResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
+
+
+export function App() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const urlFilter = searchParams.get('filter') ?? ''
+
+  const [filter, setFilter] = useState(urlFilter)
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+
+      return params
+    })
+  }
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+
+  const { data: TagsResponse, isLoading } = useQuery<TagsResponse>({
+    queryKey: ['get-tags', urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
+      const data = await response.json()
+
+      await new Promise(resolve => setTimeout(resolve, 500))
+      
+      return data
+    },
+    placeholderData: keepPreviousData,
+  })
+
+  if(isLoading) {
+    return null
+  }
+
+  return (
+    <div className="py-10 space-x-8">
+        <div>
+          <Header />
+          <Tabs />
+        </div>
+
+        <main className="max-w-[1200px] mx-auto space-y-5">
+          <div className="flex items-center gap-3">
+            <h1 className="text-xl font-bold ">Tags</h1>
+            <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1">
+
+              <Plus className="size-3"/>  
+              Create New
+              
+            </button>
+
+          </div>
+
+          <div className="flex items-center justify-between">
+            
+              <div className="flex items-center">
+                <Input variant='filter'>
+                  <Search className="size-3" />
+                  <Control 
+                    placeholder="Search tags..." 
+                    onChange={e => setFilter(e.target.value)}
+                    value={filter}
+                  />
+                </Input>
+                <Button onClick={handleFilter}>
+                  <Filter className="size-3" />
+                  Filter
+                </Button>
+              </div>
+              <Button>
+                <FileDown></FileDown>
+                Export
+              </Button>
+          </div>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead>Tags</TableHead>
+                <TableHead>Amount of videos</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+            {TagsResponse?.data.map((tag) => {
+              return (
+                <TableRow key={tag.id}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tag.amountOfVideos} video(s)
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+          </Table>
+
+          {TagsResponse && <Pagination pages={TagsResponse.pages} items={TagsResponse.items} page={page} />}
+
+        </main>
+    </div>
+  )
+}
